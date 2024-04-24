7ad0bb1ed2b69385eea14d1c3e32b9dd88200538
baudoin <baudoinvicbolingo@gmail.com>
2024-04-24 14:21:41 +0200

    fxing about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -110,13 +110,7 @@ function About() {
       <div className="bg-body-tertiary py-5">
         <div className="container">
           <h2 className="text-center mb-5">Our Winners</h2>
-          <div className="row g-4">
-            {persons.map((person) => (
-              <div key={person.id} className="col-md-4">
-                <img src={person.img} className="img-fluid" alt="" />
-              </div>
-            ))}
-          </div>
+         */}
         </div>
       </div>
     </div>
