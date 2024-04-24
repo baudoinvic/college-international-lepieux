fc7d911978114a76e6c1d7aca617aa1c868cbb71
baudoin <baudoinvicbolingo@gmail.com>
2024-04-24 15:00:29 +0200

    fixed ask questions

--- src/components/FaqAccordion/FaqAccordion.jsx
+++ src/components/FaqAccordion/FaqAccordion.jsx
@@ -18,43 +18,40 @@ function FaqAccordion() {
         </p>
         <Accordion defaultActiveKey="" flush>
           <Accordion.Item eventKey="0">
-            <Accordion.Header>First question ?</Accordion.Header>
+            <Accordion.Header>How do I enroll my child ?</Accordion.Header>
             <Accordion.Body>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo
-              ducimus quod qui eius nobis quam temporibus similique fugit?
-              Molestias, illo, maxime nulla aliquid ut pariatur corrupti
-              aspernatur dolor assumenda quos officia eos?
+              To enroll your child at [School Name], please visit our admissions
+              page on our website or contact our admissions office directly. Our
+              team will guide you through the enrollment process and answer any
+              questions you may have.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1">
-            <Accordion.Header>Second question ?</Accordion.Header>
+            <Accordion.Header>What classes we offer?</Accordion.Header>
             <Accordion.Body>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo
-              ducimus quod qui eius nobis quam temporibus similique fugit?
-              Molestias, illo, maxime nulla aliquid ut pariatur corrupti
-              aspernatur dolor assumenda quos officia eos?
+              We offer different subjects like math, science, language arts,
+              social studies, arts, and physical education to give students a
+              well-rounded education.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2">
-            <Accordion.Header>Third question ?</Accordion.Header>
+            <Accordion.Header>
+              How does Lepieux international school keep students safe and
+              happy?
+            </Accordion.Header>
             <Accordion.Body>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo
-              ducimus quod qui eius nobis quam temporibus similique fugit?
-              Molestias, illo, maxime nulla aliquid ut pariatur corrupti
-              aspernatur dolor assumenda quos officia eos?
+              We have rules, security, counseling, and health programs in place
+              to make sure all students feel safe and supported.
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="3">
-            <Accordion.Header>Fourth question ?</Accordion.Header>
+            <Accordion.Header>
+              How can I know about school events and news?
+            </Accordion.Header>
             <Accordion.Body>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Molestiae ab deserunt eaque nostrum voluptate, facilis ratione quo
-              ducimus quod qui eius nobis quam temporibus similique fugit?
-              Molestias, illo, maxime nulla aliquid ut pariatur corrupti
-              aspernatur dolor assumenda quos officia eos?
+              Check our website, follow us on social media, or sign up for
+              emails to stay updated. We might also send notices home or call
+              you with important updates.
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
