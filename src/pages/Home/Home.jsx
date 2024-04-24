559a7b70edad0a69fcfb2f077904c5a20edfc200
baudoin <baudoinvicbolingo@gmail.com>
2024-04-24 14:32:39 +0200

    start fixing home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -32,73 +32,104 @@ const blogs = [
 
 function Home() {
   return (
-    <div className='home-page'>
-        <header className='h-100 min-vh-100 d-flex align-items-center text-light'>
-            <div className='container d-flex flex-column align-items-center'>
-                <h2>Welcome To</h2>
-                <h1 className='text-center fw-semibold'>Lepieux international school</h1>
-                <p>High school education excellence we are happy to connect with you in whatever way you are most comfortable</p>
-                <div className='d-flex flex-column flex-sm-row align-items-center'>
-                    <Link to="/courses">
-                        <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Our Courses</button>
-                    </Link>
-                    <Link to="/contact">
-                        <button type='button' className='btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Contact Us</button>
-                    </Link>
-                </div>
+    <div className="home-page">
+      <header className="h-100 min-vh-100 d-flex align-items-center text-light">
+        <div className="container d-flex flex-column align-items-center">
+          <h2>Welcome To</h2>
+          <h1 className="text-center fw-semibold">
+            Lepieux international school
+          </h1>
+          <p>
+            High school education excellence we are happy to connect with you in
+            whatever way you are most comfortable
+          </p>
+          <div className="d-flex flex-column flex-sm-row align-items-center">
+            <Link to="/courses">
+              <button
+                type="button"
+                className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0"
+              >
+                Our Courses
+              </button>
+            </Link>
+            <Link to="/contact">
+              <button
+                type="button"
+                className="btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0"
+              >
+                Contact Us
+              </button>
+            </Link>
+          </div>
+        </div>
+      </header>
+
+      <div className="py-5">
+        <ChooseSection />
+      </div>
+
+      <div className="py-5 bg-light">
+        <div className="container">
+          <div className="row d-flex align-items-center justify-content-around">
+            <div className="col-lg-5">
+              <h2 className="text-capitalize">2024 start courses</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus, placeat.
+              </p>
+              <Link to="/courses">
+                <button
+                  type="button"
+                  className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0"
+                >
+                  Learn More
+                </button>
+              </Link>
             </div>
-        </header>
-
-        <div className="py-5">
-            <ChooseSection />
-        </div>
-
-        <div className='py-5 bg-light'>
-            <div className="container">
-                <div className='row d-flex align-items-center justify-content-around'>
-                    <div className='col-lg-5'>
-                        <h2 className='text-capitalize'>2024 start courses</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, placeat.</p>
-                        <Link to="/courses">
-                            <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Learn More</button>
-                        </Link>
-                    </div>
-                    <div className='col-lg-5 mt-5 mt-lg-0'>
-                        <img src={StartCoursesImg} className='img-fluid' alt="" />
-                    </div>
-                </div>
+            <div className="col-lg-5 mt-5 mt-lg-0">
+              <img
+                src="https://pbs.twimg.com/media/F-ZOGvhWgAEm9J2?format=jpg&name=4096x4096"
+                className="img-fluid"
+                alt=""
+              />
             </div>
+          </div>
         </div>
+      </div>
 
-        <div className="py-5">
-            <FaqAccordion />
-        </div>
+      <div className="py-5">
+        <FaqAccordion />
+      </div>
 
-        <div className='blog-section text-light py-5'>
-            <div className='container d-flex flex-column align-items-center'>
-                <h2 className='text-center text-capitalize mb-5'>Latest on the blog</h2>
-                <div className='row g-4'>
-                    {blogs.map((blog) => (
-                        <div key={blog.id} className='col-md-6 col-lg-4'>
-                            <Link to="/blog" className='text-decoration-none'>
-                                <Card className='h-100 shadow scale-hover-effect'>
-                                    <Card.Img variant="top" src={blog.img} />
-                                    <Card.Body className='p-md-5'>
-                                        <Card.Title>{blog.title}</Card.Title>
-                                        <Card.Text>{blog.description}</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-                <Link to="/blog">
-                    <button type='button' className='btn btn-danger btn-lg mt-5'>Read More Blogs</button>
+      <div className="blog-section text-light py-5">
+        <div className="container d-flex flex-column align-items-center">
+          <h2 className="text-center text-capitalize mb-5">
+            Latest on the blog
+          </h2>
+          <div className="row g-4">
+            {blogs.map((blog) => (
+              <div key={blog.id} className="col-md-6 col-lg-4">
+                <Link to="/blog" className="text-decoration-none">
+                  <Card className="h-100 shadow scale-hover-effect">
+                    <Card.Img variant="top" src={blog.img} />
+                    <Card.Body className="p-md-5">
+                      <Card.Title>{blog.title}</Card.Title>
+                      <Card.Text>{blog.description}</Card.Text>
+                    </Card.Body>
+                  </Card>
                 </Link>
-            </div>
+              </div>
+            ))}
+          </div>
+          <Link to="/blog">
+            <button type="button" className="btn btn-danger btn-lg mt-5">
+              Read More Blogs
+            </button>
+          </Link>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home;
