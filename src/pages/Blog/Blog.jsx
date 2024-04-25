ccb5b365657e8cf5bd1a48bb3ed051c7a9d74940
baudoin <baudoinvicbolingo@gmail.com>
2024-04-25 12:41:07 +0200

    commit

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -67,35 +67,43 @@ const blogs = [
 
 function Blog() {
   return (
-    <div className='blog-page'>
-        <header className='height-75'>
-            <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
-                <h1 className='text-center fw-semibold'>Blog</h1>
-                <p className='text-center w-75 mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.</p>
-            </div>
-        </header>
-
-        <div className='bg-body-tertiary py-5'>
-            <div className="container">
-                <div className="row g-4">
-                    {blogs.map((blog) => (
-                        <div key={blog.id} className='col-md-6 col-lg-4'>
-                            <Link to="/blog" className='text-decoration-none'>
-                                <Card className='h-100 shadow scale-hover-effect bg-dark text-light border-0'>
-                                    <Card.Img variant="top" src={blog.img} />
-                                    <Card.Body className='p-md-5 d-flex flex-column align-items-center'>
-                                        <Card.Title className='fs-2 mb-4'>{blog.title}</Card.Title>
-                                        <Card.Text className='text-center'>{blog.description}</Card.Text>
-                                    </Card.Body>
-                                </Card>
-                            </Link>
-                        </div>
-                    ))}
-                </div>
-            </div>
+    <div className="blog-page">
+      <header className="height-75">
+        <div className="container h-100 d-flex flex-column align-items-center justify-content-center text-light">
+          <h1 className="text-center fw-semibold">Blog</h1>
+          <p className="text-center w-75 mb-5">
+            Welcome to our blog on building a career in computing! Explore the
+            diverse opportunities and pathways available in the ever-evolving
+            world of technology,alongside other related fields.
+          </p>
         </div>
+      </header>
+
+      <div className="bg-body-tertiary py-5">
+        <div className="container">
+          <div className="row g-4">
+            {blogs.map((blog) => (
+              <div key={blog.id} className="col-md-6 col-lg-4">
+                <Link to="/blog" className="text-decoration-none">
+                  <Card className="h-100 shadow scale-hover-effect bg-dark text-light border-0">
+                    <Card.Img variant="top" src={blog.img} />
+                    <Card.Body className="p-md-5 d-flex flex-column align-items-center">
+                      <Card.Title className="fs-2 mb-4">
+                        {blog.title}
+                      </Card.Title>
+                      <Card.Text className="text-center">
+                        {blog.description}
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Blog;
