4b4c7173b5fa0ab04d08ed6e217e5582ba417b51
baudoin <baudoinvicbolingo@gmail.com>
2024-04-27 15:00:50 +0200

    added styling on short courses

--- src/pages/Short/Shortcouses.jsx
+++ src/pages/Short/Shortcouses.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Short.css";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 import HealthcareCourseImg from "../../utils/images/healthcare-course.jpg";
 import LawCourseImg from "../../utils/images/law-course.jpg";
@@ -11,17 +12,11 @@ const courses = [
   {
     id: 1,
     img: "https://media.istockphoto.com/id/691703568/photo/engineering-students-with-their-teacher-writing-on-the-board.jpg?s=612x612&w=0&k=20&c=hDlN563oKwpDNyRSQXqWidPlFNBTbK6_jGtJYOts1Yk=",
-    title: "Electronics",
+    title: "Networking and telecomunication",
     description:
       "This is the electronics course. It covers topics such as circuit design, electronic components, and circuit analysis.",
   },
-  {
-    id: 2,
-    img: "https://media.istockphoto.com/id/942164000/photo/group-of-young-people-in-technical-vocational-training-with-teacher.jpg?s=612x612&w=0&k=20&c=Pi2UgPyqiijHDRt9m2th9Bqm5SCCwwOF6zg2NpAZnHI=",
-    title: "General electricity",
-    description:
-      "This course covers fundamental principles and applications of general electricity, including circuits, voltage, current, resistance, and power.",
-  },
+ 
   {
     id: 3,
     img: "https://media.istockphoto.com/id/1048994466/photo/indian-young-girl-sewing-stock-image.jpg?s=612x612&w=0&k=20&c=8YRmaNa8okGasnkmN8rrqDLgM-uExpSR1P3XKJkvDvk=",
@@ -32,7 +27,7 @@ const courses = [
   {
     id: 4,
     img: "https://media.istockphoto.com/id/1204089501/photo/industrial-design-student-building-furniture-with-the-help-of-his-teacher.jpg?s=612x612&w=0&k=20&c=7n3hBHNxMd0dOELxSvxTuOINe5qQd1-1XJheTNQzfaE=",
-    title: "Construction",
+    title: "Adjustment and welding",
     description:
       "This course covers the principles and techniques of construction, including building materials, structural systems, construction methods, and project management",
   },
@@ -40,21 +35,21 @@ const courses = [
   {
     id: 6,
     img: "https://media.istockphoto.com/id/600697264/photo/medical-business-meeting.jpg?s=612x612&w=0&k=20&c=5WQMnOkFXFq8EPu19h879iQBc448JnB9Tf6DSAw5fS4=",
-    title: "Sales and Management",
+    title: "Adult rehabilitation school",
     description:
       "This course focuses on commercial and management skills, including marketing, sales, finance, human resources, and business operations.",
   },
   {
     id: 7,
     img: "https://media.istockphoto.com/id/827810840/photo/group-of-student-mechanics-working-on-car-engine-with-hood-up.jpg?s=612x612&w=0&k=20&c=WrklsPki6PkwqQ_i7dMXz5SKgzLtdbET-S6JZV_TGeY=",
-    title: "Mechanical",
+    title: "Domestic and industrial installation",
     description:
       "This course covers principles of mechanics, including statics, dynamics, kinematics, and mechanical systems, and their applications in engineering and technology.",
   },
   {
     id: 8,
     img: "https://media.istockphoto.com/id/1519355186/photo/i-need-this-data.jpg?s=612x612&w=0&k=20&c=ivfi9uNZlwCQt842_4JyBK-9Y1egC1xQp3-qZ-gX1jU=",
-    title: "Veterinary and Agronomy",
+    title: "Masonry and plumbing",
     description:
       "This course explores veterinary science and agronomy, covering topics such as animal health, nutrition, breeding, crop production, and soil management.",
   },
@@ -76,18 +71,16 @@ function Shortcourses() {
       <div className="container py-5">
         <div className="row g-4">
           {courses.map((course) => (
-            <div key={course.id} className="col-lg-6">
-              <Card className="text-white shadow scale-hover-effect">
-                <Card.Img src={course.img} />
-                <Card.ImgOverlay className="d-flex flex-column align-items-center justify-content-center p-md-5">
-                  <Card.Title className="fs-1 text-danger">
-                    {course.title}
-                  </Card.Title>
-                  <Card.Text className="text-center">
-                    {course.description}
-                  </Card.Text>
-                </Card.ImgOverlay>
-              </Card>
+            <div key={course.id} className="col-md-6 col-lg-4">
+              <Link to="/blog" className="text-decoration-none">
+                <Card className="h-100 shadow scale-hover-effect">
+                  <Card.Img variant="top" src={course.img} />
+                  <Card.Body className="p-md-5">
+                    <Card.Title>{course.title}</Card.Title>
+                    <Card.Text>{course.description}</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Link>
             </div>
           ))}
         </div>
