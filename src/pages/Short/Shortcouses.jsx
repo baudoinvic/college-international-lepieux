cc7a1c0bfcc1d3e68eb3cc6d8e18c1982a4163af
baudoin <baudoinvicbolingo@gmail.com>
2024-04-27 15:31:19 +0200

    change all pictures

--- src/pages/Short/Shortcouses.jsx
+++ src/pages/Short/Shortcouses.jsx
@@ -11,22 +11,22 @@ import FaqAccordion from "../../components/FaqAccordion/FaqAccordion";
 const courses = [
   {
     id: 1,
-    img: "https://media.istockphoto.com/id/691703568/photo/engineering-students-with-their-teacher-writing-on-the-board.jpg?s=612x612&w=0&k=20&c=hDlN563oKwpDNyRSQXqWidPlFNBTbK6_jGtJYOts1Yk=",
+    img: "https://www.chartercollege.edu/sites/default/files/iStock-800305712.jpg",
     title: "Networking and telecomunication",
     description:
       "This is the electronics course. It covers topics such as circuit design, electronic components, and circuit analysis.",
   },
- 
+
   {
     id: 3,
-    img: "https://media.istockphoto.com/id/1048994466/photo/indian-young-girl-sewing-stock-image.jpg?s=612x612&w=0&k=20&c=8YRmaNa8okGasnkmN8rrqDLgM-uExpSR1P3XKJkvDvk=",
+    img: "https://media.istockphoto.com/id/657489642/photo/happy-seamstress-at-work.jpg?s=612x612&w=0&k=20&c=yBKMm8cJ5l3TPvtafIzLUQlZ6ARJQC8vQH_EyYXp_xE=",
     title: "cut and seam ",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam odio eos distinctio ducimus! Modi quaerat dolores illo accusamus maxime cupiditate culpa minus animi repellat autem!",
   },
   {
     id: 4,
-    img: "https://media.istockphoto.com/id/1204089501/photo/industrial-design-student-building-furniture-with-the-help-of-his-teacher.jpg?s=612x612&w=0&k=20&c=7n3hBHNxMd0dOELxSvxTuOINe5qQd1-1XJheTNQzfaE=",
+    img: "https://media.istockphoto.com/id/697520720/photo/better-than-any-man.jpg?s=612x612&w=0&k=20&c=gijyjuxgAvC1ZogRaD0Mg92H0pvzX0aXAi5CIgjs458=",
     title: "Adjustment and welding",
     description:
       "This course covers the principles and techniques of construction, including building materials, structural systems, construction methods, and project management",
@@ -34,21 +34,21 @@ const courses = [
 
   {
     id: 6,
-    img: "https://media.istockphoto.com/id/600697264/photo/medical-business-meeting.jpg?s=612x612&w=0&k=20&c=5WQMnOkFXFq8EPu19h879iQBc448JnB9Tf6DSAw5fS4=",
+    img: "https://crrfstgobain.fr/wp-content/uploads/2021/04/crrf-image-333-1536x1024.jpg",
     title: "Adult rehabilitation school",
     description:
       "This course focuses on commercial and management skills, including marketing, sales, finance, human resources, and business operations.",
   },
   {
     id: 7,
-    img: "https://media.istockphoto.com/id/827810840/photo/group-of-student-mechanics-working-on-car-engine-with-hood-up.jpg?s=612x612&w=0&k=20&c=WrklsPki6PkwqQ_i7dMXz5SKgzLtdbET-S6JZV_TGeY=",
+    img: "https://media.istockphoto.com/id/1657566602/photo/two-construction-workers-working-together-on-an-apartment-renovation.jpg?s=612x612&w=0&k=20&c=9U8I8t_7rljrVhCCQHACecXooE0_iJ7bTvlXp7WDNXs=",
     title: "Domestic and industrial installation",
     description:
       "This course covers principles of mechanics, including statics, dynamics, kinematics, and mechanical systems, and their applications in engineering and technology.",
   },
   {
     id: 8,
-    img: "https://media.istockphoto.com/id/1519355186/photo/i-need-this-data.jpg?s=612x612&w=0&k=20&c=ivfi9uNZlwCQt842_4JyBK-9Y1egC1xQp3-qZ-gX1jU=",
+    img: "https://media.istockphoto.com/id/1616202154/photo/construction-worker-using-modern-concrete-wall-grinding-machine-together-with-vacuum-pump-to.jpg?s=612x612&w=0&k=20&c=vI5Jx1sRyCzArBCuXH7ubvbIJ_g9OXVLfLnh0uECc5c=",
     title: "Masonry and plumbing",
     description:
       "This course explores veterinary science and agronomy, covering topics such as animal health, nutrition, breeding, crop production, and soil management.",
