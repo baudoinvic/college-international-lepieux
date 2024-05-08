dcc0491acc4afb01b6d247e546e6c7394949021b
baudoin <baudoinvicbolingo@gmail.com>
2024-05-08 18:57:06 +0200

    done with about translations

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,107 +1,41 @@
-import React from 'react';
-import './About.css';
-import { Link } from 'react-router-dom';
-import AboutUsSectionImg from '../../utils/images/about-us-section-img.jpg';
-import ChooseSection from '../../components/ChooseSection/ChooseSection';
-import Person1 from '../../utils/images/person1.jpg';
-import Person2 from '../../utils/images/person2.jpg';
-import Person3 from '../../utils/images/person3.jpg';
-import Person4 from '../../utils/images/person4.jpg';
-import Person5 from '../../utils/images/person5.jpg';
-import Person6 from '../../utils/images/person6.jpg';
-import Person7 from '../../utils/images/person7.jpg';
-import Person8 from '../../utils/images/person8.jpg';
-import Person9 from '../../utils/images/person9.jpg';
 
-const persons = [
-    {
-        id: 1,
-        img: [Person1]
-    },
-    {
-        id: 2,
-        img: [Person2]
-    },
-    {
-        id: 3,
-        img: [Person3]
-    },
-    {
-        id: 4,
-        img: [Person4]
-    },
-    {
-        id: 5,
-        img: [Person5]
-    },
-    {
-        id: 6,
-        img: [Person6]
-    },
-    {
-        id: 7,
-        img: [Person7]
-    },
-    {
-        id: 8,
-        img: [Person8]
-    },
-    {
-        id: 9,
-        img: [Person9]
-    },
-];
+
+import React from "react";
+import "./About.css";
+import { Link } from "react-router-dom";
+import AboutUsSectionImg from "../../utils/images/about-us-section-img.jpg";
+import ChooseSection from "../../components/ChooseSection/ChooseSection";
+import { useTranslation } from "react-i18next";
 
 function About() {
+  const { t } = useTranslation();
+
   return (
     <div className="about-page">
       <header className="height-75">
         <div className="container h-100 d-flex flex-column align-items-center justify-content-center text-light">
-          <h1 className="text-center fw-semibold">About Us</h1>
-          <p className="text-center w-75 mb-5">
-            College international Lepieux is a leading secondary educational
-            institution located in the city. from Kalemie, Democratic Republic
-            of Congo, dedicated to providing high quality technical services
-            education to students. Our institute aims to equip students with the
-            skills and knowledge to excel in various technical fields, promoting
-            personal and professional growth. We offering a comprehensive
-            curriculum, state-of-the-art facilities and supportive learning
-            environment to ensure our students receive the best
-            education possible.
-          </p>
+          <h1 className="text-center fw-semibold">{t("about.title")}</h1>
+          <p className="text-center w-75 mb-5">{t("about.description")}</p>
         </div>
       </header>
 
       <div className="container my-5">
         <div className="row">
           <div className="col-lg-6 d-flex flex-column justify-content-center mb-4 mb-lg-0">
-            <h2 className="mb-4 mb-lg-5">Study with us</h2>
-            <p>
-              Educational Offerings Discover the rich tapestry of learning
-              experiences waiting for you within our institution. Proficient
-              Learning Initiatives Experience and the transformative power of
-              expert-led education, shaping minds and futures.
-            </p>
-            <p className="mb-4 mb-lg-5">
-              Academic Pathways Embark on a transformative journey through our
-              Academic Pathways, where students are empowered to explore diverse
-              subjects.
-            </p>
+            <h2 className="mb-4 mb-lg-5">{t("about.studyWithTitle")}</h2>
+            <p>{t("about.studyDescription1")}</p>
+            <p className="mb-4 mb-lg-5">{t("about.studyDescription2")}</p>
             <Link to="/contact">
               <button
                 type="button"
                 className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0"
               >
-                Contact Us
+                {t("about.contactButton")}
               </button>
             </Link>
           </div>
           <div className="col-lg-6 d-flex justify-content-center">
-            <img
-              src="https://pbs.twimg.com/media/F-ZOGvhWgAEm9J2?format=jpg&name=4096x4096"
-              className="img-fluid w-75"
-              alt=""
-            />
+            <img src={AboutUsSectionImg} className="img-fluid w-75" alt="" />
           </div>
         </div>
       </div>
@@ -110,9 +44,7 @@ function About() {
         <ChooseSection />
       </div>
 
-      <div className="bg-body-tertiary py-5">
-       
-      </div>
+      <div className="bg-body-tertiary py-5"></div>
     </div>
   );
 }
