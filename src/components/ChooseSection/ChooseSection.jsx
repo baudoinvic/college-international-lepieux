999c57d7ccd12514baa5c93f9fe9766e40250019
baudoin <baudoinvicbolingo@gmail.com>
2024-05-08 14:26:08 +0200

    keep translate

--- src/components/ChooseSection/ChooseSection.jsx
+++ src/components/ChooseSection/ChooseSection.jsx
@@ -1,13 +1,115 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+// import React from 'react';
+// import { Card } from 'react-bootstrap';
+
+// function ChooseSection() {
+//   return (
+//     <div>
+//       <div className="container">
+//         <h2 className="text-center mb-5">
+//           Why choose Collège international le pieux ?
+//         </h2>
+//         <div className="row g-4">
+//           <div className="col-lg-4">
+//             <Card className="d-flex align-items-center border-0 h-100">
+//               <div className="mt-3">
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   width="48"
+//                   height="48"
+//                   fill="#dc3545"
+//                   class="bi bi-trophy-fill"
+//                   viewBox="0 0 16 16"
+//                 >
+//                   <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
+//                 </svg>
+//               </div>
+//               <Card.Body>
+//                 <Card.Title className="text-center text-capitalize mb-3">
+//                   Education Excellence
+//                 </Card.Title>
+//                 <Card.Text className="text-center">
+//                   "We are happy to connect with you in whatever way you are most
+//                   comfortable. We offer the best education to our students,
+//                   ensuring they have access to a diverse range of learning
+//                   resources and opportunities.
+//                 </Card.Text>
+//               </Card.Body>
+//             </Card>
+//           </div>
+//           <div className="col-lg-4">
+//             <Card className="d-flex align-items-center border-0 h-100">
+//               <div className="mt-3">
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   width="48"
+//                   height="48"
+//                   fill="#dc3545"
+//                   class="bi bi-emoji-smile-fill"
+//                   viewBox="0 0 16 16"
+//                 >
+//                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8" />
+//                 </svg>
+//               </div>
+//               <Card.Body>
+//                 <Card.Title className="text-center text-capitalize mb-3">
+//                   We keep our students happy
+//                 </Card.Title>
+//                 <Card.Text className="text-center">
+//                   our students they are always happy because with the lectures
+//                   with strong knowledge and experiences. our students they are
+//                   happy according the education they use to get and the way they
+//                   use to Study
+//                 </Card.Text>
+//               </Card.Body>
+//             </Card>
+//           </div>
+//           <div className="col-lg-4">
+//             <Card className="d-flex align-items-center border-0 h-100">
+//               <div className="mt-3">
+//                 <svg
+//                   xmlns="http://www.w3.org/2000/svg"
+//                   width="48"
+//                   height="48"
+//                   fill="#dc3545"
+//                   class="bi bi-geo-alt-fill"
+//                   viewBox="0 0 16 16"
+//                 >
+//                   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
+//                 </svg>
+//               </div>
+//               <Card.Body>
+//                 <Card.Title className="text-center text-capitalize mb-3">
+//                   our Mission
+//                 </Card.Title>
+//                 <Card.Text className="text-center">
+//                   Lepieux International Technical Institute is committed to
+//                   providing quality technical education, fostering innovation,
+//                   and promoting entrepreneurship among its students. We aim to
+//                   empower individuals with the skills and knowledge necessary to
+//                   thrive in a rapidly evolving global  economy.
+//                 </Card.Text>
+//               </Card.Body>
+//             </Card>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ChooseSection;
+
+import React from "react";
+import { Card } from "react-bootstrap";
+import { useTranslation } from "react-i18next";
 
 function ChooseSection() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <div className="container">
-        <h2 className="text-center mb-5">
-          Why choose Collège international le pieux ?
-        </h2>
+        <h2 className="text-center mb-5">{t("choose_section.heading")}</h2>
         <div className="row g-4">
           <div className="col-lg-4">
             <Card className="d-flex align-items-center border-0 h-100">
@@ -17,7 +119,7 @@ function ChooseSection() {
                   width="48"
                   height="48"
                   fill="#dc3545"
-                  class="bi bi-trophy-fill"
+                  className="bi bi-trophy-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M2.5.5A.5.5 0 0 1 3 0h10a.5.5 0 0 1 .5.5c0 .538-.012 1.05-.034 1.536a3 3 0 1 1-1.133 5.89c-.79 1.865-1.878 2.777-2.833 3.011v2.173l1.425.356c.194.048.377.135.537.255L13.3 15.1a.5.5 0 0 1-.3.9H3a.5.5 0 0 1-.3-.9l1.838-1.379c.16-.12.343-.207.537-.255L6.5 13.11v-2.173c-.955-.234-2.043-1.146-2.833-3.012a3 3 0 1 1-1.132-5.89A33.076 33.076 0 0 1 2.5.5m.099 2.54a2 2 0 0 0 .72 3.935c-.333-1.05-.588-2.346-.72-3.935zm10.083 3.935a2 2 0 0 0 .72-3.935c-.133 1.59-.388 2.885-.72 3.935" />
@@ -25,13 +127,10 @@ function ChooseSection() {
               </div>
               <Card.Body>
                 <Card.Title className="text-center text-capitalize mb-3">
-                  Education Excellence
+                  {t("choose_section.card1.title")}
                 </Card.Title>
                 <Card.Text className="text-center">
-                  "We are happy to connect with you in whatever way you are most
-                  comfortable. We offer the best education to our students,
-                  ensuring they have access to a diverse range of learning
-                  resources and opportunities.
+                  {t("choose_section.card1.description")}
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -44,7 +143,7 @@ function ChooseSection() {
                   width="48"
                   height="48"
                   fill="#dc3545"
-                  class="bi bi-emoji-smile-fill"
+                  className="bi bi-emoji-smile-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16M7 6.5C7 7.328 6.552 8 6 8s-1-.672-1-1.5S5.448 5 6 5s1 .672 1 1.5M4.285 9.567a.5.5 0 0 1 .683.183A3.498 3.498 0 0 0 8 11.5a3.498 3.498 0 0 0 3.032-1.75.5.5 0 1 1 .866.5A4.498 4.498 0 0 1 8 12.5a4.498 4.498 0 0 1-3.898-2.25.5.5 0 0 1 .183-.683M10 8c-.552 0-1-.672-1-1.5S9.448 5 10 5s1 .672 1 1.5S10.552 8 10 8" />
@@ -52,13 +151,10 @@ function ChooseSection() {
               </div>
               <Card.Body>
                 <Card.Title className="text-center text-capitalize mb-3">
-                  We keep our students happy
+                  {t("choose_section.card2.title")}
                 </Card.Title>
                 <Card.Text className="text-center">
-                  our students they are always happy because with the lectures
-                  with strong knowledge and experiences. our students they are
-                  happy according the education they use to get and the way they
-                  use to Study
+                  {t("choose_section.card2.description")}
                 </Card.Text>
               </Card.Body>
             </Card>
@@ -71,7 +167,7 @@ function ChooseSection() {
                   width="48"
                   height="48"
                   fill="#dc3545"
-                  class="bi bi-geo-alt-fill"
+                  className="bi bi-geo-alt-fill"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6" />
@@ -79,14 +175,10 @@ function ChooseSection() {
               </div>
               <Card.Body>
                 <Card.Title className="text-center text-capitalize mb-3">
-                  our Mission
+                  {t("choose_section.card3.title")}
                 </Card.Title>
                 <Card.Text className="text-center">
-                  Lepieux International Technical Institute is committed to
-                  providing quality technical education, fostering innovation,
-                  and promoting entrepreneurship among its students. We aim to
-                  empower individuals with the skills and knowledge necessary to
-                  thrive in a rapidly evolving global  economy.
+                  {t("choose_section.card3.description")}
                 </Card.Text>
               </Card.Body>
             </Card>
