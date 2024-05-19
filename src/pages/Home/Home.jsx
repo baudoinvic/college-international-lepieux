d87aa4ac154e6556769dcfdd17ce2d4336a46826
baudoin <baudoinvicbolingo@gmail.com>
2024-05-19 13:52:03 +0200

    fixed bugs

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -166,7 +166,7 @@ function Home() {
         </div>
       </div> */}
       <div className="container mx-auto p-4">
-        {/* Page Title */}
+        
         <h1 className="text-3xl font-bold text-center mb-8">
           {t("event.title")}
         </h1>
