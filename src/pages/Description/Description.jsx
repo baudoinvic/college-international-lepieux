f6ea56fc9b3bd755804dafa07048c28d8b0d7ee6
baudoin <baudoinvicbolingo@gmail.com>
2024-05-25 15:39:07 +0200

    fixed translation

--- src/pages/Description/Description.jsx
+++ src/pages/Description/Description.jsx
@@ -38,7 +38,7 @@ const Description = () => {
               type="button"
               className="btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0 "
             >
-              Contact us
+              {t("conta.title")}
             </button>
           </Link>
         </div>
