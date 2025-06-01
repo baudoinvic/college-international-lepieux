806b8e53a4419d8634cc239face89d3336ebedec
baudoin <baudoinvicbolingo@gmail.com>
2025-06-01 14:45:40 +0300

    translated the about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,8 +1,6 @@
 
-
 import React from "react";
 import { Link } from "react-router-dom";
-// import ChooseSection from "../../components/ChooseSection/ChooseSection";
 import { useTranslation } from "react-i18next";
 import campus from "../../utils/images/campus.png";
 
